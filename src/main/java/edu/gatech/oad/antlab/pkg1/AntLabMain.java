22381e1acdc53cf00f00b08fc2f8c13cb04967db
zhoufr17 <32785746+zhoufr17@users.noreply.github.com>
2018-09-04 19:39:30 -0400

    deleted extra p5 reference

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -77,13 +77,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   Person5 p5 = new Person5("Frank Zhoou");
                   toPrint += p5.toString("fzhou65");
 
-
-                  Person5 p5 = new Person5("P5");
-                  toPrint += p5.toString("gburdell5");
-
-
         System.out.println(toPrint);
-
 
     }
 
