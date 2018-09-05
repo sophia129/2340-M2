c72bf235257b79ac12949abf07b438d0fbdc9a7f
vsabhnani <vmsabhnani@gmail.com>
2018-09-05 19:50:44 -0400

    Add testing comment

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -65,7 +65,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p2.toString("abhamani6");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
-		  Person3 p3 = new Person3("Viren Sabhnani");
+		  Person3 p3 = new Person3("Viren Sabhnani"); //this comment is for testing
 		  toPrint += p3.toString("vsabhnani3");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
